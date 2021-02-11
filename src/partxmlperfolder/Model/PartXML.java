15b8ff58f637d3b1bc7a69a0dcf22b5fcc763159
Admnistrador <Admnistrador@DESKTOP-B4ERJ5H>
2021-02-11 11:15:30 -0300

    added PartXML

---
+++ src/partxmlperfolder/Model/PartXML.java
@@ -0,0 +1,70 @@
+package partxmlperfolder.Model;
+
+import fileManager.FileManager;
+import fileManager.StringFilter;
+import fileManager.XML;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.w3c.dom.NodeList;
+
+public class PartXML {
+
+    public static Map<String, String> filters = null;
+    public static File[] xmlFiles = null;
+
+    /**
+     * -- Se tiver 1 filtro encontrado
+     * --- Cria pasta com o numero do produto(valor do mapa) se nao existir
+     * --- Salva arquivo
+     * -- Se tiver mais de 1 filtro
+     * --- Cria pasta se não existir "Mais de 1 produto"
+     * --- Salva arquivo
+     * -- Se não
+     * --- Cria pasta se não existir "Nenhum produto cadastrado"
+     * --- Salva arquivo
+     */
+    public static void partXMLs() {
+        for (File xmlFile : xmlFiles) {
+            try {
+                XML xml = new XML(xmlFile);
+                NodeList products = xml.doc.getElementsByTagName("Discriminacao");
+
+                if (products.item(0) != null) {
+                    String product = products.item(0).getTextContent();
+
+                    List<String> foundFilters = new ArrayList<>();
+
+                    filters.forEach((filter, name) -> {
+                        StringFilter sf = new StringFilter(filter);
+                        //Se tiver o filtro na Discriminacao
+                        if (sf.filterOfString(product)) {
+                            foundFilters.add(name);
+                        }
+                    });
+
+                    if (foundFilters.size() == 1) {
+                        saveFileOnFolder(xmlFile, foundFilters.get(0));
+                    } else if (foundFilters.size() > 1) {
+                        saveFileOnFolder(xmlFile, "Mais de 1 produto");
+                    } else {
+                        saveFileOnFolder(xmlFile, "Nenhum produto encontrado");
+                    }
+                }
+
+            } catch (Exception ex) {
+                throw new Error(ex);
+            }
+        }
+    }
+
+    private static void saveFileOnFolder(File xmlFile, String newFolder) {
+        // Cria pasta se não existir "Mais de 1 produto"
+        File folder = new File(xmlFile.getParent() + "\\" + newFolder);
+        folder.mkdirs();
+
+        // Salva arquivo
+        FileManager.save(folder, xmlFile.getName(), FileManager.getText(xmlFile));
+    }
+}
--- src/partxmlperfolder/PartXMLPerFolder.java
+++ src/partxmlperfolder/PartXMLPerFolder.java
@@ -5,33 +5,45 @@ import fileManager.Selector;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Map;
 import javax.swing.JOptionPane;
 import partxmlperfolder.Model.Config;
+import partxmlperfolder.Model.PartXML;
+import static partxmlperfolder.Model.PartXML.filters;
+import static partxmlperfolder.Model.PartXML.xmlFiles;
 
 public class PartXMLPerFolder {
 
-    /**
-     * -Pegar pasta com XMLs
-     * -Percorrer arquivos XMLs da pasta
-     * - Para cada arquivo
-     * -- Pega texto do campo 'Discriminacao'
-     * --
-     * -- Percorre mapa de filtros
-     * --- Se
-     */
     public static void main(String[] args) {
         try {
-            int option = JOptionPane.showOptionDialog(null, "Arquivo CSV de configuração necessário!\nVocê deseja obter o CSV de configuração de exemplo ou deseja escolher o arquivo pronto para uso?", "Obter csv ou escolher csv?", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[]{"Obter CSV Exemplo","Escolher CSV"}, "Obter CSV Exemplo");
-            if(option == 0){
+            int option = JOptionPane.showOptionDialog(null, "Arquivo CSV de configuração necessário!\nVocê deseja obter o CSV de configuração de exemplo ou deseja escolher o arquivo pronto para uso?", "Obter csv ou escolher csv?", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[]{"Obter CSV Exemplo", "Escolher CSV"}, "Obter CSV Exemplo");
+            if (option == 0) {
                 Config.getFileExemple();
                 JOptionPane.showMessageDialog(null, "Arquivo de exemplo salvo na área de trabalho");
-            }else{
-                File file =  Selector.selectFile(null, "CSV", ".csv");
-                if(file != null && file.exists()){
-                    Map<String, String>  filters = Config.getConfigFilters(file);
-                    System.out.println("");
-                }else{
+            } else {
+                File file = Selector.selectFile(null, "CSV", ".csv");
+                if (file != null && file.exists()) {
+                    //Pega mada de filtros
+                    filters = Config.getConfigFilters(file);
+
+                    //Verifica se tem filtros
+                    if (filters.size() > 0) {
+                        //Escolhe pasta com XML
+                        JOptionPane.showMessageDialog(null, "Escolha a seguir a pasta com os XMLs para separar:");
+
+                        File xmlFolder = Selector.selectFolder(null);
+                        if (xmlFolder != null && xmlFolder.exists()) {
+                            xmlFiles = xmlFolder.listFiles();
+                            
+                            
+                            PartXML.partXMLs();
+                            JOptionPane.showMessageDialog(null, "XMLs salvos! Verifique as pastas");
+                        } else {
+                            throw new Error("Pasta inválida!");
+                        }
+                    } else {
+                        throw new Error("Arquivo de configuração vazio, nada irá acontecer...");
+                    }
+                } else {
                     throw new Error("Arquivo inválido!");
                 }
             }
@@ -47,6 +59,7 @@ public class PartXMLPerFolder {
         FileManager.save(new File(System.getProperty("user.home")) + "\\Desktop\\JavaError.txt", getStackTrace(e));
         JOptionPane.showMessageDialog(null, "Arquivo de erro java salvo na área de trabalho 'JavaError.txt'. Use se precisar contatar o programador.", "Erro!", JOptionPane.ERROR_MESSAGE);
     }
+
     private static String getStackTrace(Exception e) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
